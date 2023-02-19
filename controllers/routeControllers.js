3f5c56cbddbc89f47eb9e96e3c23c1b06093e9f7
abhay2002-pro <abhayray2002@gmail.com>
2023-02-19 21:43:04 +0530

    added API for creating new role

--- controllers/routeControllers.js
+++ controllers/routeControllers.js
@@ -2,11 +2,28 @@ import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import Role from "../models/Role.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
-export const getRoles = catchAsyncError(async (req, res, next) => {
-  const roles = Role.findAll();
+export const createRole = catchAsyncError(async (req, res, next) => {
+  const name = req.body.name;
+  const scopes = req.body.scopes;
+  
+  const exists = await Role.findOne({ where: { name: name } });
+  if (exists) return next(new ErrorHandler("Role exists already", 404));
 
+  const role = await Role.create({ name, scopes });
   res.status(200).json({
-    success: true,
-    roles: roles,
+    status: true,
+    content: {
+      data: role,
+    },
+  });
+});
+
+export const getRoles = catchAsyncError(async (req, res, next) => {
+  const roles = await Role.findAll({});
+  res.status(200).json({
+    status: true,
+    content: {
+      data: roles,
+    },
   });
 });
--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getRoles } from "../controllers/routeControllers.js";
+import { getRoles, createRole } from "../controllers/routeControllers.js";
 
 const router = express.Router();
 
-router.route("/role").post(getRoles);
+router.route("/role").post(createRole);
 router.route("/role").get(getRoles);
 
 export default router;
