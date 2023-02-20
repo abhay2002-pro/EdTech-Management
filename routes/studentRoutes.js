d7b54c9a86f1667cc99eec1ed53f043a799631a3
abhay2002-pro <abhayray2002@gmail.com>
2023-02-20 22:39:47 +0530

    added authorisation

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { createStudent, getAllStudents } from "../controllers/studentControllers.js";
+import { isAuthorised } from "../middlewares/authorization.js";
 import { createStudentValidation } from "../validators/studentValidators.js";
 
 const router = express.Router();
 
-router.route("/student").post(createStudentValidation, createStudent);
-router.route("/student").get(getAllStudents);
+router.route("/student").post(createStudentValidation, isAuthorised,createStudent);
+router.route("/student").get(isAuthorised, getAllStudents);
 
 export default router;
