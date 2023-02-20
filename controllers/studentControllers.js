18a70f784c822d0ab1fb093e410d1733fdb31fef
abhay2002-pro <abhayray2002@gmail.com>
2023-02-20 15:54:18 +0530

    added scope check in createSchool API

---
+++ controllers/studentControllers.js
@@ -0,0 +1,26 @@
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
+import Student from "../models/Student.js";
+import Role from "../models/Role.js";
+import ErrorHandler from "../utils/errorHandler.js";
+
+export const createStudent = catchAsyncError(async (req, res, next) => {
+  const role = await Role.findOne({where: {id : req.roleId}});
+  const scopes = role.scopes;
+
+  if(!scopes.includes("school-create")){
+    return next(new ErrorHandler("User doesn't have access to this resource", 404));
+  }
+
+  const { name, userId, schoolId } = req.body;
+
+  const exists = await Student.findOne({ where: { name: name } });
+  if (exists) return next(new ErrorHandler("Student exists already", 404));
+
+  const student = await Student.create({ name, userid: userId, schoolid: schoolId });
+  res.status(200).json({
+    status: true,
+    content: {
+      data: student,
+    },
+  });
+});
--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createSchool, getAllSchools } from "../controllers/schoolControllers.js";
+import {
+  createSchool,
+  getAllSchools,
+} from "../controllers/schoolControllers.js";
+import { isAuthorised } from "../middlewares/authorization.js";
 
 const router = express.Router();
 
-router.route("/school").post(createSchool);
-router.route("/school").get(getAllSchools);
+router.route("/school").post(isAuthorised, createSchool);
+router.route("/school").get(isAuthorised, getAllSchools);
 
 export default router;
