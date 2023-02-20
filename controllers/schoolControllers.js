43c49f9233b919f313f4f16679b6bfad9b385aef
abhay2002-pro <abhayray2002@gmail.com>
2023-02-20 15:59:01 +0530

    added scope check in getAllSchools

--- controllers/schoolControllers.js
+++ controllers/schoolControllers.js
@@ -1,9 +1,17 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import School from "../models/School.js";
+import Role from "../models/Role.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import lookup from "country-code-lookup";
 
 export const createSchool = catchAsyncError(async (req, res, next) => {
+  const role = await Role.findOne({where: {id : req.roleId}});
+  const scopes = role.scopes;
+
+  if(!scopes.includes("school-create")){
+    return next(new ErrorHandler("User doesn't have access to this resource", 404));
+  }
+
   const { name, city, state, country } = req.body;
 
   const exists = await School.findOne({ where: { name: name } });
@@ -21,6 +29,13 @@ export const createSchool = catchAsyncError(async (req, res, next) => {
 });
 
 export const getAllSchools = catchAsyncError(async (req, res, next) => {
+  const role = await Role.findOne({where: {id : req.roleId}});
+  const scopes = role.scopes;
+
+  if(!scopes.includes("school-get")){
+    return next(new ErrorHandler("User doesn't have access to this resource", 404));
+  }
+  
   const schools = await School.findAll({});
 
   res.status(200).json({
