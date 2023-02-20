72f6f522578769896f58b35a489f00d4b38e62c5
abhay2002-pro <abhayray2002@gmail.com>
2023-02-20 22:28:41 +0530

    added validation for create student

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createStudent, getAllStudents } from "../controllers/studentControllers.js";
+import { createStudentValidation } from "../validators/studentValidators.js";
 
 const router = express.Router();
 
-router.route("/student").post(createStudent);
+router.route("/student").post(createStudentValidation, createStudent);
 router.route("/student").get(getAllStudents);
 
 export default router;
